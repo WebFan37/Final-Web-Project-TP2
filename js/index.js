336fc28d2581563b931789dbf119049775b281da
Janne Elllison <jeanneel2000@gmail.com>
2023-05-17 20:58:58 -0400

    Projet final

---
+++ js/index.js
@@ -0,0 +1,417 @@
+
+///////////////////////////// LES VARIABLES DU QUIZ ///////
+//////////////////////////////////////////////////////////
+
+//Nombre de question justes ou  correctes
+let nombreQuestionCorrect = 0;
+
+//Numéro de la question courante
+let noQuestionCourante = 0;
+
+//LOL garder ça
+let setActuel = questionSet1;
+
+
+
+//==========SOURCE MÉDIA ET SON ==============///
+
+let musiqueQuiz = new Audio ('Medias/musique-quiz.mp3');
+let sonRessi = new Audio ('Medias/son-succes.mp3');
+let sonEchec = new Audio ('Medias/son-echec.mp3');
+//===========================================///
+
+
+//---------------  SECTION BARRE D'AVANCEMENT --------
+//Barre d'avancement quiz
+let barreAvancement = document.querySelector('.barre-avancement');
+
+// largeur de la barre d'avancement 
+let largeurDeBarre = 0;
+
+// cible du largeur pour la progression
+let cibleBarre = 0;
+//----------------------------------------------------
+
+
+
+
+//-------------SECTION DES QUIZ ----------------
+//Page d'intro 
+let pageIntro = document.querySelector(".page-intro");
+
+//Où mettre le quiz
+let placePourQuiz = document.querySelector(".quiz");
+
+// Fin du quiz
+let finQuiz = document.querySelector(".resultat");
+
+//Section pour le quiz ????? ////////////////////
+let sectioQuiz = document.querySelector("section");
+let positionX = 100;
+
+//Conteneurs pour la les titres et des choix
+let titreQuestion = document.querySelector(".titre-questions");
+
+//--------------------------------
+
+
+//=========TIMER=====   NE MARCHE PAS//
+// let timeLeft = 30;
+
+// let elem = document.querySelector(".bouton-timer");
+// let timerID = setInterval(countdown, 1000);
+//==================//
+
+
+
+//============ SAUVEGARDE DES DONNÉES LOCAL STORAGE ===========//
+//===============================================//
+let historique = recuperationHistorique();
+
+function recuperationHistorique(){
+    let chaineDeDonne = localStorage.getItem("les-scores-historiques");
+
+    return JSON.parse(chaineDeDonne) || [];
+}
+
+
+
+//=========THÈME=====//
+let themeJourNuit;
+let caseTheme = document.querySelector("#id-check-box");
+//===================//
+
+
+//bouton commencer quiz
+let boutonCommencer = document.querySelector(".section-commencer");
+
+//Recommencer un autre set 
+let boutonDeRestart = document.querySelector(".bouton-recommencer");
+
+//CURSEUR PERSONNALISÉ PART 1
+let curseur = document.querySelector(".curseur");
+
+//bouton retour au menu principal
+let boutonRetour = document.querySelector(".bouton-retour");
+
+
+
+
+
+//////=============================//////
+/////========= ÉVÉNEMENTS ========//////
+
+//Ce qui se passe après animation du titre
+boutonCommencer.addEventListener("click", commencerSet1);
+
+//Ce qui se passe après on clique sur Recommencer
+boutonDeRestart.addEventListener('click', recommencer);
+
+//CURSEUR PERSONNALISÉ PART 2
+document.addEventListener("mousemove", bougerCurseur);
+
+//case thème nuit
+caseTheme.addEventListener("click", changerTheme);
+
+
+
+
+
+//////=============================//////
+/////========= FONCTIONS ========//////
+function changerTheme (event){
+    //selecteur root
+    let root = document.querySelector(":root");
+
+    if (caseTheme.checked == true){
+        // Test
+        //console.log("Texte 1");
+        root.style.setProperty("--couleur-fond", "#005c60")
+        
+    } else{
+        //test
+        //console.log("Texte 5");
+        root.style.setProperty("--couleur-fond", "#008e96");
+    }
+
+}
+
+
+//CommencerSet1
+function commencerSet1(){
+   //=======IMPORTANT LOCAL STORAGE ====//
+   //AJOUTER LA DATE ACTUELLE ET LA RÉPONSE EN % //
+   historique.push({ date: new Date().toLocaleDateString("fr-CA"), reponses: [] });
+   
+localStorage.setItem("les-scores-historiques", JSON.stringify(historique));
+    //==================================//
+
+
+
+   //enlève la page intro
+    pageIntro.style.display = "none";
+    
+    //enlève l'ecouteur qui gere affichage du question
+    boutonCommencer.removeEventListener("click", commencerSet1);
+
+    //enlève le conteneur fin quiz pour l'instant ????
+    finQuiz.style.display = "none";
+
+    //scene quiz
+    placePourQuiz.style.display = "flex";
+
+    //PROCHAIN FONCTION
+    afficherQuestions();
+    
+    //Joue la musique quiz
+    musiqueQuiz.play();
+
+    
+
+
+    
+}
+
+//commencer le quiz
+function afficherQuestions(){
+    
+    //ajouter l'écouteur du retour
+    boutonRetour.addEventListener("click", retourAuDebut);
+    
+
+    //=======AJOUT DES QUESTIONS======//
+
+    //Les questions dans la section questionnaire.js
+    let lesQuestions = setActuel[noQuestionCourante];
+    
+    //Mettre le textes des questions
+    titreQuestion.innerText = lesQuestions.titre;
+
+
+
+    //=======AJOUT DES QUESTIONS JUSQU'À LA FIN======//
+    let notreChoix;
+
+    //ajouter les questions suite par suite
+    for (let index = 0; index < lesQuestions.choix.length; index++){
+        //création de la section et l'ajout des choix
+        notreChoix = document.createElement("div");
+    
+        //Mettre dans ??
+        notreChoix.innerText = lesQuestions.choix[index];
+
+        //Affectation de chaque choix ??
+        notreChoix.indexChoix = index;
+
+        //Affichage
+        titreQuestion.append(notreChoix);
+
+        //VÉRIFICATION DES RÉPONSES PARTIE 1
+        notreChoix.addEventListener("mousedown", verification);
+    }
+
+
+    //===== ANIMATION BARRE D'AVANCEMENT ====/
+
+    //valeur de sa position
+    positionX = 100;
+
+    //la section RAF
+    requestAnimationFrame(animerSectionBarre);
+
+    //partir la plus difficile
+    cibleBarre = (noQuestionCourante + 1) / questionSet1.length * 100;
+
+    //Commencer l'animation de notre barre d'avancement
+    requestAnimationFrame(animationBarre);
+
+    //=======COMMENCER TIMER ========//
+    // function countdown(){
+    //     if (timeLeft == 0){
+    //         mettreProchainsQuestion();
+    //     } else {
+    //         elem.innerHTML = timeLeft;
+    //         timeLeft--;
+    //     }
+    // }
+
+}
+
+ //===== ANIMATION BARRE D'AVANCEMENT FONCTION 1 ====/
+function animationBarre(){
+    //augmentation du largeur
+    largeurDeBarre += 1;
+
+    //problem!!!!!!!!!!
+    barreAvancement.style.width = largeurDeBarre + "vw";
+
+    //Partie largeur cible
+    if(largeurDeBarre < cibleBarre){
+        requestAnimationFrame(animationBarre);
+    }
+
+}
+
+//===== ANIMATION BARRE D'AVANCEMENT FONCTION 2 ====/
+function animerSectionBarre(){
+    positionX -= 1;
+    sectioQuiz.style.transform = `translateX(${positionX})`;
+
+    if (positionX > 0){
+        requestAnimationFrame(animerSectionBarre);
+    }
+
+}
+
+//VERIFICATION DES REPONSES PAR L'UTILISATEUR
+function verification (event){
+    
+    // !!!! LOCAL STORAGE //
+    //====================//
+    historique[historique.length - 1].reponses.push(event.target.indexChoix)
+
+    localStorage.setItem("les-scores-historiques", JSON.stringify(historique));
+
+//===========================//
+
+
+
+    let choixFinal = event.target;
+
+    if (choixFinal.indexChoix == questionSet1[noQuestionCourante].reponse){
+        // choixFinal.classList.add('succes');
+        // console.log("CORRECT " + nombreQuestionCorrect);
+        nombreQuestionCorrect += 1;
+        sonRessi.play();
+    }
+    else{
+        // choixFinal.classList.add('echec');
+        // console.log("WRONG" + nombreQuestionCorrect);
+        sonEchec.play();
+    }
+
+    choixFinal.addEventListener('click', mettreProchainsQuestion);
+
+}
+
+//METTRE LES PROCHAINS QUESTIONS
+function mettreProchainsQuestion (event){
+
+    //l'ajout des nombre de question courante pour passer a la prochaine question
+    noQuestionCourante++;
+
+    //Est-ce qu'il reste encore des questions?
+    if (noQuestionCourante < questionSet1.length){
+        afficherQuestions();
+    } else{
+        afficherResultat();
+    }
+}
+
+//AFFICHAGE DU RESULTAT
+    function afficherResultat(){
+        //enleve le quiz
+        placePourQuiz.style.display = "none";
+        
+        // ============SECTION LOCAL STORAGE ========//
+        //=========Il faut montrer les scores en % =======//
+        historique = recuperationHistorique();
+
+        //savoir les parties jouées
+        let lesPartiesJouées = historique.length;
+
+        let scoreResultat = document.createElement('section');
+    
+        //AFFICHAGE DES LOCAL STORAGE ET RÉPONSES
+        scoreResultat.innerHTML = `<p>VOUS AVEZ JOUÉ ${lesPartiesJouées} PARTIES</p>`;
+        scoreResultat.innerHTML += `<p> VOTRE RÉPONSE: ${nombreQuestionCorrect + " / 10" + "\n" + ((nombreQuestionCorrect/10)*100)+"%"}</p>`;
+    for (let partie of historique) {
+        scoreResultat.innerHTML += ` Histoire: ${partie.date} <br>`;
+    }
+
+        //animation
+        scoreResultat.classList.add("animationResultat");
+
+        //append et insertion
+        finQuiz.append(scoreResultat);
+
+        //Fin quiz display 
+        finQuiz.style.display = "flex";
+
+        //Si on veut recommencer
+        boutonDeRestart.addEventListener('click', recommencer);
+       
+
+       
+
+        
+    }
+
+//SI ON CLIQUE SUR RETOUR, ON RETOURNE AU DEBUT DU QUIZ
+function retourAuDebut(){
+    //Reset des variables
+    noQuestionCourante = 0;
+    nombreQuestionCorrect = 0;
+    largeurDeBarre = 0; 
+    cibleBarre = 0; 
+
+    //Test
+    console.log("TEST POUR SAVOIR S'IL A MARCHÉ " + ((345+234)-34) * 10);
+
+    //enleve le conteneur du quiz???
+    placePourQuiz.style.display = "none";
+    
+    //afficher la page principal?????
+    pageIntro.style.display = "flex";
+
+    //restart
+    boutonCommencer.addEventListener("click", commencerSet1);
+
+    //Pas de musique
+    musiqueQuiz.pause();
+
+
+}
+
+//RECOMMENCER LES QUESTIONS
+function recommencer(){
+   console.log("Test pour verification rapide");
+
+   //Reset des variables
+   noQuestionCourante = 0;
+   nombreQuestionCorrect = 0;
+   largeurDeBarre = 0; 
+   cibleBarre = 0; 
+
+   //enleve la page intro
+    pageIntro.style.display = "none";
+    
+    //enleve l'ecouteur qui gere affichage du question
+    boutonDeRestart.removeEventListener('click', recommencer);
+
+    //enleve le conteneur fin quiz
+    finQuiz.style.display = "none";
+
+    //scene quiz
+    placePourQuiz.style.display = "flex";
+
+    //PROCHAIN FONCTION
+    afficherQuestions();
+}
+
+/*FONCTION POUR LE CURSEUR (PART 3)*/
+function bougerCurseur(event){
+    //Test dans Console en premier
+    //console.log("client X, Y : ", event.clientX, event.clientY );
+
+    //suivre le curseur
+    curseur.style.translate = event.clientX + 'px ' + event.clientY + 'px'; 
+    
+
+    //Window matchMedia 
+    if (window.matchMedia("(hover: hover)").matches){
+        document.querySelector("body").style.cursor = "none";
+    } else {
+        document.querySelector("body").style.cursor = "auto";
+    }
+}
---
+++ js/questionnaire.js
@@ -0,0 +1,133 @@
+let questionSet1 = [
+    {
+        titre:"Quelle musique de Clementi a un lien direct avec l'ouverture de la flûte enchanté? ",
+        choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+        reponse: 1
+
+    },
+    {
+        titre:"Quelle lettre de l'alphabet latin correspond à la lettre cyrillique П? ",
+        choix:['B', 'R', 'P', 'N'],
+        reponse: 2
+
+    },
+    {
+        titre:"De 1912 à 1948, quelle catégorie secondaire a gagné la médaille d'or dans Olympique ?  ",
+        choix:['Sport', 'Math', 'Art', 'Science'],
+        reponse: 2
+
+    },
+    {
+        titre:"Combien d'imposteur ont réclamé d'être le fils de Marie Antoinette ?",
+        choix:['20', '50', '10', '100'],
+        reponse: 3
+
+    },
+    {
+        titre:"Au 3e siècle après Jésus-Christ, comment appelle-t-on les Japonais? ",
+        choix:['Nihon Jin', 'Jin', 'Nippon', 'Wa-Jin'],
+        reponse: 3
+
+    },
+    {
+        titre:"Quelle décision a un impact littéraire que Vietnam et Corée ont fait au 20e siècle?",
+        choix:['Abandon du Han Tu et Hanja ', 'Invention écriture simple', 'Invention du Chu Nôm', 'Simplification du Hanja'],
+        reponse: 0
+
+    },
+    {
+        titre:"En quel période la Chine a inventé le poudre à canon ?   ",
+        choix:['7e Siècle', '8e Siècle', '9e Siècle', '10e Siècle'],
+        reponse: 2
+
+    },
+    {
+        titre:"Quel est le nom original de la ville de Montréal ? ",
+        choix:['Mont-Royal', 'Hochelaga', 'Maisonneuve', 'Mont-Laurier'],
+        reponse: 0
+
+    },
+    {
+        titre:"Quel est le nom original de la ville du New York? ",
+        choix:['New Netherland', 'Paumanok', 'New Armsterdam', 'Quinnetuket'],
+        reponse: 2
+
+    },
+    {
+        titre:"Qui a fondé la Colombie ? ",
+        choix:['Zheng He', 'Vasco da Gama', 'Cristoforo Colombo', 'Rodrigo de Bastidas'],
+        reponse: 3
+
+    },
+    
+
+
+
+]
+
+
+//===============TROP COMPLIQUÉ À COMPRENDRE LA LOGIQUE DU SENS POUR RECOMMENCER LE DEUXIÈME SET =======//
+//------------------------- Rest In Peace ---------------//
+
+// let questionSet2 = [
+//     {
+//         titre:"? 4",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"? 6",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"? 8",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"? 210",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"?a 4hfheg ",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"? q3r4ygwdrht",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"? asdgnw",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"? asdbaert",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"?a asdf asdf  ",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+//     {
+//         titre:"? asdf afsd afs d",
+//         choix:['Sonatina Op36 No5', 'Sonate Op24 No2', 'Sonate Op47 No2', 'Sonate Op40 No2'],
+//         reponse: 2
+
+//     },
+// ]
